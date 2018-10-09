467fb2f5d6a10cedcf5ca0b5123c26e6e5141d86
Bick95 <daniel.bick@live.de>
2018-10-09 18:59:23 +0200

    Initial commit.

---
+++ Clusterer/src/clusterer/ClusteringAlgorithm.java
@@ -0,0 +1,16 @@
+package clusterer;
+
+public abstract class ClusteringAlgorithm
+{
+	public abstract boolean train();
+
+	public abstract boolean test();
+	
+	public abstract void setPrefetchThreshold(double prefetchThreshold);    
+	
+	public abstract void showTest();
+	
+	public abstract void showMembers();
+	
+	public abstract void showPrototypes();
+}
